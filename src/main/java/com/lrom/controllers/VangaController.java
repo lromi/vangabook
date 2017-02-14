69e0d1669934581839f34cea0c89389df456fe59
lromi <lromi@mail.ru>
2017-02-15 01:32:17 +0200

    Deploy

---
+++ src/main/java/com/lrom/controllers/VangaController.java
@@ -0,0 +1,24 @@
+package com.lrom.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+    public class VangaController {
+        @RequestMapping(value = "/")
+        public String index(){
+
+            return "index";
+        }
+    @RequestMapping(value= "/about", method = RequestMethod.GET)
+    public String  about(Model model) {
+
+        return "about";
+    }
+
+
+}
+

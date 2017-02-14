6477a5112a71b2c39e64887ccb18573d5388ece7
lromi <lromi@mail.ru>
2017-02-15 00:27:41 +0200

    Deploy

--- src/main/java/com/lrom/configuration/SecurityConfiguration.java
+++ src/main/java/com/lrom/configuration/SecurityConfiguration.java
@@ -5,16 +5,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-    @Configuration
-    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-//                    .and()
-//                    .authorizeRequests().antMatchers("/console/**").permitAll();
-
-            httpSecurity.csrf().disable();
-            httpSecurity.headers().frameOptions().disable();
-        }
-}
+//    @Configuration
+//    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity httpSecurity) throws Exception {
+//            httpSecurity.authorizeRequests().antMatchers("/").permitAll()
+//                   .and()
+//                   .authorizeRequests().antMatchers("/console/**").permitAll();
+//
+//            httpSecurity.csrf().disable();
+//            httpSecurity.headers().frameOptions().disable();
+//        }
+//}

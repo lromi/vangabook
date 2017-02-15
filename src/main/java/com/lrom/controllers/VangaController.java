7441736f84f620bd11b0d7bd4fd29f9e11ce282e
lromi <lromi@mail.ru>
2017-02-15 23:55:24 +0200

    Deploy

--- src/main/java/com/lrom/controllers/VangaController.java
+++ src/main/java/com/lrom/controllers/VangaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
     public class VangaController {
-        @RequestMapping(value= "/")
+        @RequestMapping(value= "/start")
         public String index(Model model){
 
             return "index";

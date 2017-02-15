68ea14b92a35730322db7204024f7d4f37952d18
lromi <lromi@mail.ru>
2017-02-15 23:41:01 +0200

    Deploy

--- src/main/java/com/lrom/controllers/VangaController.java
+++ src/main/java/com/lrom/controllers/VangaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
     public class VangaController {
-        @RequestMapping(value= "/index1")
+        @RequestMapping(value= "/")
         public String index(Model model){
 
             return "index";
--- src/main/java/com/lrom/services/impementations/OrderServiceImpl.java
+++ src/main/java/com/lrom/services/impementations/OrderServiceImpl.java
@@ -2,8 +2,6 @@ package com.lrom.services.impementations;
 
 import com.lrom.services.OrderService;
 
-/**
- * Created by lromi on 14.02.2017.
- */
+
 public class OrderServiceImpl implements OrderService {
 }

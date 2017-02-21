322fd3b35b7e5cb08b940e854bb682a66346d8d0
lromi <lromi@mail.ru>
2017-02-21 21:40:31 +0200

    Deploy

--- src/main/java/com/lrom/repository/CompetitorRepository.java
+++ src/main/java/com/lrom/repository/CompetitorRepository.java
@@ -3,13 +3,15 @@ package com.lrom.repository;
 
 import com.lrom.domain.Competitor;
 import lombok.NonNull;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-@Component
+
+@EnableJpaRepositories(basePackages="com.lrom.repository", entityManagerFactoryRef="emf")
 public interface CompetitorRepository extends CrudRepository<Competitor,Integer> {
 
     Optional<Competitor> findByUsername(@NonNull String username);

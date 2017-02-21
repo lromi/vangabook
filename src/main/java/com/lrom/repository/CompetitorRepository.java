49d113a6bc788f3426b503fda5f82d11fedf5f95
lromi <lromi@mail.ru>
2017-02-21 02:51:48 +0200

    Deploy

--- src/main/java/com/lrom/repository/CompetitorRepository.java
+++ src/main/java/com/lrom/repository/CompetitorRepository.java
@@ -4,12 +4,13 @@ package com.lrom.repository;
 import com.lrom.domain.Competitor;
 import lombok.NonNull;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CompetitorRepository extends CrudRepository<Competitor,Integer> {
 
-   Optional<Competitor> findByUsername(@NonNull String username);
+     Optional<Competitor> findByUsername(@NonNull String username);
 
 
 }

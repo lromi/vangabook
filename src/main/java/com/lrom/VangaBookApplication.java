a6b81a7c94ede0921069ef795e39544213041f3b
lromi <lromi@mail.ru>
2017-02-22 04:45:57 +0200

    Deploy

--- src/main/java/com/lrom/VangaBookApplication.java
+++ src/main/java/com/lrom/VangaBookApplication.java
@@ -2,11 +2,19 @@ package com.lrom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 
 @SpringBootApplication
 public class VangaBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VangaBookApplication.class, args);
+	}
+
+
+	@Bean
+	public PersistenceAnnotationBeanPostProcessor persistenceBeanPostProcessor() {
+		return new PersistenceAnnotationBeanPostProcessor();
 	}
 }
